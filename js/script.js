0289c729f9eba27da0db895f2ef37e5356d43ca9
Maayan-Meshulam <maayanmesh@gmail.com>
2024-12-13 08:44:26 +0200

    last

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function dropdownBtnFunc(){
+    if(document.getElementById("dropdownContent").style.display == "flex"){
+        document.getElementById("dropdownContent").style.display = "none";
+        
+    }
+    else{
+        document.getElementById("dropdownContent").style.display = "flex";
+        
+    }
+}
+
